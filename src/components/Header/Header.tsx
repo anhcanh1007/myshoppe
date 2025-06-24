5fedf07c3f5ce980b2877e6f71ee1d3f46b8bcf0
anhcanh1007 <anhcanh1007@gmail.com>
2025-06-24 19:38:41 +0700

    code Popover Header thuoc Mainlayout, tach component Popover su dung chung

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 
+import Popover from "../Popover";
 export default function Header() {
   return (
     <div className="pb-5 pt-2 bg-[linear-gradient(-180deg,#f53d2d,#f63)] text-white">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-end">
-          <div className="flex items-center py-1 hover:text-gray-300 cursor-pointer">
+          <Popover
+            className="flex items-center py-1 hover:text-gray-300 cursor-pointer"
+            renderPopover={
+              <div className="bg-white relative shadow-md rounded-sm border border-gray-200">
+                <div className="flex flex-col py-2 pr-28 pl-3">
+                  <button className="py-2 px-3 hover:text-orange-500 cursor-pointer">
+                    Tiếng Việt
+                  </button>
+                  <button className="py-2 px-3 hover:text-orange-500 cursor-pointer mt-2">
+                    English
+                  </button>
+                </div>
+              </div>
+            }
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -35,8 +50,29 @@ export default function Header() {
                 d="M19.5 8.25l-7.5 7.5-7.5-7.5"
               />
             </svg>
-          </div>
-          <div className="flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6">
+          </Popover>
+          <Popover
+            className="flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6"
+            renderPopover={
+              <div className="bg-white relative shadow-md rounded-sm border border-gray-200">
+                <Link
+                  to="/"
+                  className="block py-3 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left"
+                >
+                  Tài khoản của tôi
+                </Link>
+                <Link
+                  to="/"
+                  className="block py-3 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left"
+                >
+                  Đơn mua
+                </Link>
+                <button className="block py-3 px-4 hover:bg-slate-100 bg-white hover:text-cyan-500 w-full text-left">
+                  Đăng xuất
+                </button>
+              </div>
+            }
+          >
             <div className="w-6 h-6 mr-2 flex-shrink-0">
               <img
                 src="https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn"
@@ -45,7 +81,84 @@ export default function Header() {
               />
             </div>
             <div>duthanhduoc</div>
-          </div>
+          </Popover>
+          {/* <div
+            className="flex items-center py-1 hover:text-gray-300 cursor-pointer"
+            ref={refs.reference}
+            onMouseEnter={showPopover}
+            onMouseLeave={hidePopover}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+              />
+            </svg>
+            <FloatingPortal>
+              <AnimatePresence>
+                {open && (
+                  <motion.div
+                    ref={refs.floating}
+                    style={{
+                      position: strategy,
+                      top: y ?? 0,
+                      left: x ?? 0,
+                      width: "max-content",
+                      transformOrigin: `${middlewareData.arrow?.x}px top`,
+                    }}
+                    initial={{ opacity: 0, transform: "scale(0)" }}
+                    animate={{ opacity: 1, transform: "scale(1)" }}
+                    exit={{ opacity: 0, transform: "scale(0)" }}
+                    transition={{ duration: 0.2 }}
+                  ></motion.div>
+                )}
+                <div className="bg-white relative shadow-md rounded-sm border border-gray-200">
+                  Add commentMore actions
+                  <div className="flex flex-col py-2 px-3">
+                    <button className="py-2 px-3 hover:text-orange">
+                      Tiếng Việt
+                    </button>
+                    <button className="py-2 px-3 hover:text-orange mt-2">
+                      English
+                    </button>
+                  </div>
+                </div>
+              </AnimatePresence>
+            </FloatingPortal>
+            <span className="mx-1">Tiếng Việt</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div> */}
+          {/* <div className="flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6">
+            <div className="w-6 h-6 mr-2 flex-shrink-0">
+              <img
+                src="https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn"
+                alt="avatar"
+                className="w-full h-full object-cover rounded-full"
+              />
+            </div>
+            <div>duthanhduoc</div>
+          </div> */}
         </div>
         <div className="grid grid-cols-12 gap-4 mt-4 items-end">
           <Link to="/" className="col-span-2">
