4637de49ca2b34aa64feaa2fa4a57490ef3fc73c
anhcanh1007 <anhcanh1007@gmail.com>
2025-06-26 21:52:32 +0700

    code UI AsideFilter, Product, SortProductList

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,36 +1,34 @@
 import type { RegisterOptions, UseFormRegister } from "react-hook-form";
 import type { FormData } from "../../pages/Register/Register";
+import type { InputHTMLAttributes } from "react";
 
-interface PropsInput {
-  className: string;
-  type?: React.HTMLInputTypeAttribute;
-  placeholder: string;
+interface PropsInput extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
-  register: UseFormRegister<FormData>;
+  register?: UseFormRegister<FormData>;
   rules?: RegisterOptions<FormData>;
-  name: keyof FormData;
+  classNameInput?: string;
+  classNameError?: string;
 }
-
 export default function Input({
-  className,
-  placeholder,
   register,
   rules,
+  className,
   type,
-  errorMessage,
   name,
+  errorMessage,
+  classNameInput = "p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm",
+  classNameError = "mt-1 text-red-600 min-h-[1.25rem] text-sm",
 }: PropsInput) {
+  const resultRegister = register && name ? register(name, rules) : {};
   return (
     <div className={className}>
       <input
         type={type}
-        {...register(name, rules)}
-        className="p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm"
-        placeholder={placeholder}
+        {...resultRegister}
+        className={classNameInput}
+        placeholder={classNameInput}
       />
-      <div className="mt-1 text-red-600 min-h-[1.25rem] text-sm">
-        {errorMessage}
-      </div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+const plugin = require("@tailwindcss/line-clamp");
+
+module.exports = {
+  plugins: [require("@tailwindcss/line-clamp")],
+};
