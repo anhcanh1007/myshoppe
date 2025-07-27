05197ad3353fa7b457de582adf499fa5d285f697
anhcanh1007 <anhcanh1007@gmail.com>
2025-07-27 21:40:34 +0700

    su dung moi truong jsdom va thu vien jsdom de test function co noi dung can lay cac api tu browser

---
+++ src/ultils/__test__/auth.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "vitest";
+import {
+  clearAccessTokenFromLS,
+  getAccessTokenToLS,
+  setAccessTokenFromLS,
+} from "../auth";
+import { beforeEach } from "node:test";
+
+const access_token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NWMwMGU2YTcyYjRhZGQ5YWQzMjJkZiIsImVtYWlsIjoiYW5oM3Rlc3RAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyNS0wNy0yNlQxNTo1NzoxMi4wODJaIiwiaWF0IjoxNzUzNTQ1NDMyLCJleHAiOjE3NTM2MzE4MzJ9.c3Od2zqctsZyq3Sf6AKTWUkOb9x0kH9mHU0wWioxCJU";
+
+const refresh_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NWMwMGU2YTcyYjRhZGQ5YWQzMjJkZiIsImVtYWlsIjoiYW5oM3Rlc3RAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyNS0wNy0yNlQxNTo1NzoxMi4wODJaIiwiaWF0IjoxNzUzNTQ1NDMyLCJleHAiOjE3NjIxODU0MzJ9.m68bFq9wOCDlNTA8XnhzfMfDxhssvKPenGYgD-vP2SU";
+
+beforeEach(() => {
+  clearAccessTokenFromLS();
+});
+
+describe("setAccessTokenToLS", () => {
+  it("set access_token", () => {
+    setAccessTokenFromLS(access_token);
+    expect(getAccessTokenToLS()).toBe(access_token);
+  });
+});
+
+// describe("getAccessTokenToLS", () => {
+//   it("lay access_token", () => {
+//     expect(getAccessTokenToLS()).toBe(access_token);
+//   });
+// });
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -6,4 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer()],
+  test: {
+    environment: "jsdom",
+  },
 });
